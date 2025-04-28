80406e66c3e663ee48db38f2e9c40a10d2d4aaf8
Rusimbi Patrick <rusimbipatrick@outlook.com>
2025-04-29 00:20:43 +0200

    add configuration support

--- config/config.go
+++ config/config.go
@@ -1,1 +1,13 @@
 package config
+
+type Config struct {
+	Port         int        `toml:"port"`
+	Dir          string     `toml:"dir"`
+	AppendOnly   bool       `toml:"appendonly"`
+	ReplicaOf    string     `toml:"replicaof"`
+	RequirePass  string     `toml:"requirepass"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	// Read from TOML/YAML/JSON file
+}
